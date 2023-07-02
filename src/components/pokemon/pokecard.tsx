ed64a58dd7278e3d3715be25e9e58e4f1d783dec
Sevagen Veerasamy <sevagenv@gmail.com>
2023-07-02 21:48:01 +0400

    [feat] ADD pokemon type styling

--- src/components/pokemon/pokecard.tsx
+++ src/components/pokemon/pokecard.tsx
@@ -1,5 +1,18 @@
 import { component$, useSignal, useResource$, Resource, $ } from "@builder.io/qwik";
-import { pokecard, poketype, poketype_container, sprite, pokemon_index } from "./style.css";
+import {pokecard,
+        poketype,
+        poketype_grass,
+        poketype_water,
+        poketype_ground,
+        poketype_fire,
+        poketype_poison,
+        poketype_fairy,
+        poketype_flying,
+        poketype_normal,
+        poketype_container,
+        sprite,
+        pokemon_index,
+} from "./style.css";
 
 interface Pokemon {
   name: string;
@@ -13,9 +26,15 @@ interface PokemonType {
   };
 }
 
-interface PokemonData {
-  types: PokemonType;
-  sprite: string;
+enum POKEMON_TYPE {
+  GRASS = poketype_grass as any,
+  WATER = poketype_water as any,
+  GROUND = poketype_ground as any,
+  FIRE = poketype_fire as any,
+  POISON = poketype_poison as any,
+  FAIRY = poketype_fairy as any,
+  FLYING = poketype_flying as any,
+  NORMAL = poketype_normal as any
 }
 
 export default component$(({name, url, number}: Pokemon) => {
@@ -34,6 +53,27 @@ export default component$(({name, url, number}: Pokemon) => {
     return sprite.sprites.front_default;
   })
 
+  const determineTypeClass = (typeName: string): any => {
+    switch(typeName) {
+      case "grass":
+        return POKEMON_TYPE.GRASS;
+      case "water":
+        return POKEMON_TYPE.WATER;
+      case "ground":
+        return POKEMON_TYPE.GROUND;
+      case "fire":
+        return POKEMON_TYPE.FIRE;
+      case "poison":
+        return POKEMON_TYPE.POISON;
+      case "fairy":
+        return POKEMON_TYPE.FAIRY;
+      case "flying":
+        return POKEMON_TYPE.FLYING;
+      default:
+        return POKEMON_TYPE.NORMAL
+    }
+  }
+
   return (
     <div class={pokecard}>
       <Resource
@@ -49,7 +89,7 @@ export default component$(({name, url, number}: Pokemon) => {
         onResolved={(item) => {
           return (
             <ul class={poketype_container}>
-              {item.map((type) => <li class={poketype}>{type.type.name}</li>)}
+              {item.map((type) => <li class={determineTypeClass(type.type.name)}>{type.type.name}</li>)}
             </ul>
         )}}
       />
--- src/components/pokemon/style.css.ts
+++ src/components/pokemon/style.css.ts
@@ -1,4 +1,4 @@
-import { style } from 'styled-vanilla-extract/qwik';
+import { style, styled } from 'styled-vanilla-extract/qwik';
 
 export const pokecard = style({
   width: "300px",
@@ -13,6 +13,54 @@ export const pokecard = style({
 export const poketype = style({
   color: "black",
   listStyle: "none",
+})
+
+export const poketype_grass = style({
+  color: "white",
+  listStyle: "none",
+  backgroundColor: "green",
+})
+
+export const poketype_ground = style({
+  color: "white",
+  listStyle: "none",
+  backgroundColor: "brown",
+})
+
+export const poketype_fire = style({
+  color: "white",
+  listStyle: "none",
+  backgroundColor: "red",
+})
+
+export const poketype_poison = style({
+  color: "white",
+  listStyle: "none",
+  backgroundColor: "purple",
+})
+
+export const poketype_fairy = style({
+  color: "white",
+  listStyle: "none",
+  backgroundColor: "pink",
+})
+
+export const poketype_flying = style({
+  color: "white",
+  listStyle: "none",
+  backgroundColor: "yellow",
+})
+
+export const poketype_water = style({
+  color: "white",
+  listStyle: "none",
+  backgroundColor: "blue",
+})
+
+export const poketype_normal = style({
+  color: "white",
+  listStyle: "none",
+  backgroundColor: "brown",
 })
 
 export const poketype_container = style({
