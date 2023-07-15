d0505ea269d62cbc26501d8a701b5b9e8b02d992
Sevagen Veerasamy <sevagenv@gmail.com>
2023-07-15 12:13:53 +0400

    [feat] Allow css override for loading animation

--- src/components/pokemon/pokeballLoading.tsx
+++ src/components/pokemon/pokeballLoading.tsx
@@ -3,12 +3,13 @@ import styles from './pokeball.css?inline';
 
 interface PokeballLoading {
   bottom?: boolean;
+  className?: string;
 }
 
-export default component$(({bottom = false}: PokeballLoading) => {
+export default component$(({bottom = false, className}: PokeballLoading) => {
   useStylesScoped$(styles)
   return (
-    <div class={`wrapper ${bottom ? "bottom-poke" : ""}`}>
+    <div class={`wrapper ${className} ${bottom ? "bottom-poke" : ""}`}>
       <div class="pokeball">
       </div>
     </div>
