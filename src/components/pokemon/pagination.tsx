7080d8660f8791064617b6bfa9e1392756f4f580
Sevagen Veerasamy <sevagenv@gmail.com>
2023-07-23 10:21:51 +0400

    [bugfix] Fix pagination

--- src/components/pokemon/pagination.tsx
+++ src/components/pokemon/pagination.tsx
@@ -11,16 +11,19 @@ interface Pagination {
 
 export default component$(({pages, offset, totalPages}: Pagination) => {
   useStylesScoped$(styles);
+
   return (
     <div class="pagination_container">
-      <select class="pagination_select" name="pages" onChange$={(event: any) => offset.value = (event.target.value - 1) * 12}>
-        {pages.map((item: any) => <option value={item} selected={item === (offset.value/12) + 1 ? true : false}>{item.toString()}</option>)}
+      <select class="pagination_select" name="pages" onChange$={(event: any) => offset.value = (event.target.value - 1) * 16}>
+        {pages.map((item: any) => <option value={item} selected={item === (offset.value/16) + 1 ? true : false}>{item.toString()}</option>)}
       </select>
       <p class="pagination_current"> of {totalPages} pages</p>
-      <button onClick$={() => offset.value-=12}>
-        <Left />
-      </button>
-      <button onClick$={() => offset.value+=12}>
+      { offset.value > 0 &&
+        <button onClick$={() => offset.value-=16 }>
+          <Left />
+        </button>
+      }
+      <button onClick$={() => offset.value+=16}>
         <Right />
       </button>
     </div>
