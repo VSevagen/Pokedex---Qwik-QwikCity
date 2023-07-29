0b375f13ce0ce5661551b978fbd790daff7d3504
Sevagen Veerasamy <sevagenv@gmail.com>
2023-07-29 18:30:21 +0400

    [feat] ADD theme toggle button

--- src/components/pokemon/searchbar.tsx
+++ src/components/pokemon/searchbar.tsx
@@ -38,17 +38,33 @@ export default component$(({initialPokemon, errorSignal}: SearchBar) => {
             searchTerm.value = event.target.value;
           }}
         />
-        <button
-          class="submit-button"
-          onClick$={ async () => {
-            const data = await fetchPokemonByName(searchTerm.value?.toLowerCase(), errorSignal);
-            await isMobile() && window.scrollTo({ top: 100, behavior: "smooth"})
-            initialPokemon.value = data.id;
-            errorSignal.value = false;
-          }}
-        >
-          <Pokeball class="pokeball-search-button" />
-        </button>
+        <div class="button-wrapper">
+          <button
+            onClick$={() => {
+              // TO REWORK. HANDLE THROUGH CONTEXT
+              if(localStorage?.getItem('theme') === "dark") {
+                localStorage?.setItem('theme', 'light');
+                document.documentElement.className = localStorage.theme
+              } else {
+                localStorage?.setItem('theme', 'dark');
+                document.documentElement.className = localStorage.theme
+              }
+            }}
+          >
+            Toggle theme
+          </button>
+          <button
+            class="submit-button"
+            onClick$={ async () => {
+              const data = await fetchPokemonByName(searchTerm.value?.toLowerCase(), errorSignal);
+              await isMobile() && window.scrollTo({ top: 100, behavior: "smooth"})
+              initialPokemon.value = data.id;
+              errorSignal.value = false;
+            }}
+          >
+            <Pokeball class="pokeball-search-button" />
+          </button>
+        </div>
       </section>
     </>
   );
